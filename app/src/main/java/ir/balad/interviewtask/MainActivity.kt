ffcd7a213667eda708e10241ddb7acf0a4613ed4
Sepehr Akhoundi <sepehrakhoundi1812@gmail.com>
2022-06-12 15:29:52 +0430

    implement item list.

--- app/src/main/java/ir/balad/interviewtask/MainActivity.kt
+++ app/src/main/java/ir/balad/interviewtask/MainActivity.kt
@@ -2,7 +2,10 @@ package ir.balad.interviewtask
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import ir.balad.interviewtask.adapter.DataAdapter
 import ir.balad.interviewtask.databinding.ActivityMainBinding
+import ir.balad.interviewtask.model.DataModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +20,24 @@ class MainActivity : AppCompatActivity() {
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initialView()
+    }
+
+    private fun initialView() {
+        val data = DataModel.generateMockList()
+        val gridLayoutManager = GridLayoutManager(this, 2).apply {
+            spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int) =
+                    // if position of item is dividable by 3 use two spans, otherwise use just one of them
+                    if (position.plus(1).mod(3) == 0) 2 else 1
+            }
+        }
+
+        with(binding.rvItemList) {
+            layoutManager = gridLayoutManager
+            adapter = DataAdapter(data)
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/ir/balad/interviewtask/model/DataModel.kt
+++ app/src/main/java/ir/balad/interviewtask/model/DataModel.kt
@@ -1,3 +1,31 @@
 package ir.balad.interviewtask.model
 
-data class DataModel(val title: String, val color: Int)
+import android.graphics.Color
+import kotlin.random.Random
+
+data class DataModel(val title: String, val color: Int) {
+
+    companion object {
+
+        /** Generates a mocked list with default title and a random color. */
+        fun generateMockList(): List<DataModel> {
+            val list = mutableListOf<DataModel>()
+
+            repeat(12) {
+                list.add(
+                    DataModel(
+                        "Title",
+                        Color.argb(
+                            255,
+                            Random.nextInt(256),
+                            Random.nextInt(256),
+                            Random.nextInt(256)
+                        )
+                    )
+                )
+            }
+
+            return list
+        }
+    }
+}
