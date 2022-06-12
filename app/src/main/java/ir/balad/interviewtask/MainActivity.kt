83f657163360fa51357afcde073fc1411bae4805
Sepehr Akhoundi <sepehrakhoundi1812@gmail.com>
2022-06-12 14:39:24 +0430

    initialize MainActivity.

--- app/src/main/java/ir/balad/interviewtask/MainActivity.kt
+++ app/src/main/java/ir/balad/interviewtask/MainActivity.kt
@@ -2,10 +2,25 @@ package ir.balad.interviewtask
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ir.balad.interviewtask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // region of properties
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    // END of region of properties
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
