514674d0e4987decdb190c764f1a352400c15926
Sepehr Akhoundi <sepehrakhoundi1812@gmail.com>
2022-06-13 15:07:18 +0430

    add javadoc for custom exceptions.

--- MusicPlayer/src/main/java/ir/balad/musicplayer/exception/IllegalOperationException.kt
+++ MusicPlayer/src/main/java/ir/balad/musicplayer/exception/IllegalOperationException.kt
@@ -1,3 +1,10 @@
 package ir.balad.musicplayer.exception
 
+
+/**
+ * [Exception] is thrown when user wants to do a not-allowed operation in MusicPlayer;
+ *  e.g. pausing a music that is already paused.
+ *
+ *  @author Sepi 6/13/22
+ */
 class IllegalOperationException(override val message: String?) : Exception(message)
